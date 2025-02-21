478ccc00fddb128dd10c552a05a9d80a555ad7bc
dipaadipati <adipatirzky@gmail.com>
2025-02-21 18:04:15 +0800

    feat: add loading spin while fetching

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,14 @@
 	let selectedEditId = $state(-1);
 	let isEdit = $derived(selectedEditId > 0 ? true : false);
 	let usersData = $state([...data.users]);
+	let res = $state();
 
 	const addUser = async () => {
 		const formData = new FormData();
 		formData.append('name', nameInput);
 		formData.append('age', ageInput.toString());
 
-		const res = fetch('/api/users', {
+		res = fetch('/api/users', {
 			method: 'POST',
 			body: formData
 		}).then(() => {
@@ -34,7 +35,7 @@
 		formData.append('id', id!.toString());
 		formData.append('isDone', 'true');
 
-		const res = fetch('/api/users', {
+		res = fetch('/api/users', {
 			method: 'PATCH',
 			body: formData
 		}).then(() => {
@@ -60,7 +61,7 @@
 		formData.append('name', nameInput);
 		formData.append('age', ageInput.toString());
 
-		const res = fetch('/api/users', {
+		res = fetch('/api/users', {
 			method: 'PATCH',
 			body: formData
 		}).then(() => {
@@ -78,7 +79,7 @@
 		const formData = new FormData();
 		formData.append('id', id!.toString());
 
-		const res = fetch('/api/users', {
+		res = fetch('/api/users', {
 			method: 'DELETE',
 			body: formData
 		}).then(() => {
@@ -90,73 +91,84 @@
 <div
 	class="h-screen w-screen flex items-center justify-center bg-gradient-to-b from-blue-800 to-cyan-600 text-white"
 >
-	<table class="border-1 text-center border-separate [border-spacing:0.75rem]">
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Age</th>
-				<th>Status</th>
-				<th>Actions</th>
-				<th>Edit</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each usersData as user}
+	{#await res}
+		<svg class="size-5 animate-spin" viewBox="0 0 24 24">
+			<circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" fill="none"></circle>
+			<path d="M4 12a8 8 0 018-8" stroke="currentColor" stroke-width="4" stroke-linecap="round"
+			></path>
+		</svg>
+	{:then value}
+		<table class="border-1 text-center border-separate [border-spacing:0.75rem]">
+			<thead>
 				<tr>
-					<td>{user.name}</td>
-					<td>{user.age}</td>
+					<th>Name</th>
+					<th>Age</th>
+					<th>Status</th>
+					<th>Actions</th>
+					<th>Edit</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each usersData as user}
+					<tr>
+						<td>{user.name}</td>
+						<td>{user.age}</td>
+						<td>
+							{#if user.isDone}
+								<span class="bg-green-200 text-green-700 p-1 rounded-lg">Done</span>
+							{:else}
+								<span class="bg-yellow-600 text-white p-1 rounded-lg">On Working</span>
+							{/if}
+						</td>
+						<td>
+							{#if !user.isDone}
+								<button
+									class="border-1 p-1 rounded-lg hover:bg-green-300 cursor-pointer"
+									onclick={() => handleDoneAction(user.id)}>Done</button
+								>
+							{/if}
+							<button
+								class="border-1 p-1 rounded-lg border-red-600 text-red-600 hover:bg-red-600 hover:text-white cursor-pointer"
+								onclick={() => deleteUser(user.id)}>Delete</button
+							>
+						</td>
+						<td>
+							<button
+								class={`border-1 p-1 rounded-lg cursor-pointer ${selectedEditId === user.id ? 'bg-gray-600' : 'hover:bg-gray-300'}`}
+								onclick={() => handleSelectedEdit(user.id, user.name, user.age)}>Select</button
+							>
+						</td>
+					</tr>
+				{/each}
+				<tr>
 					<td>
-						{#if user.isDone}
-							<span class="bg-green-200 text-green-700 p-1 rounded-lg">Done</span>
-						{:else}
-							<span class="bg-yellow-600 text-white p-1 rounded-lg">On Working</span>
-						{/if}
+						<input type="text" bind:value={nameInput} class="border-1 w-40" />
 					</td>
 					<td>
-						{#if !user.isDone}
+						<input type="text" bind:value={ageInput} class="border-1 w-10 text-center" />
+					</td>
+					<td></td>
+					<td>
+						{#if !isEdit}
 							<button
-								class="border-1 p-1 rounded-lg hover:bg-green-300 cursor-pointer"
-								onclick={() => handleDoneAction(user.id)}>Done</button
+								onclick={addUser}
+								class="border-1 p-1 rounded-lg hover:bg-amber-300 hover:text-amber-800 cursor-pointer"
+								>Add</button
+							>
+						{:else}
+							<button
+								onclick={updateUser}
+								class="border-1 p-1 rounded-lg hover:bg-yellow-300 cursor-pointer">Edit</button
 							>
 						{/if}
-						<button
-							class="border-1 p-1 rounded-lg border-red-600 text-red-600 hover:bg-red-600 hover:text-white cursor-pointer"
-							onclick={() => deleteUser(user.id)}>Delete</button
-						>
-					</td>
-					<td>
-						<button
-							class={`border-1 p-1 rounded-lg cursor-pointer ${selectedEditId === user.id ? 'bg-gray-600' : 'hover:bg-gray-300'}`}
-							onclick={() => handleSelectedEdit(user.id, user.name, user.age)}>Select</button
-						>
 					</td>
 				</tr>
-			{/each}
-			<tr>
-				<td>
-					<input type="text" bind:value={nameInput} class="border-1 w-40" />
-				</td>
-				<td>
-					<input type="text" bind:value={ageInput} class="border-1 w-10 text-center" />
-				</td>
-				<td></td>
-				<td>
-					{#if !isEdit}
-						<button
-							onclick={addUser}
-							class="border-1 p-1 rounded-lg hover:bg-amber-300 hover:text-amber-800 cursor-pointer"
-							>Add</button
-						>
-					{:else}
-						<button
-							onclick={updateUser}
-							class="border-1 p-1 rounded-lg hover:bg-yellow-300 cursor-pointer">Edit</button
-						>
-					{/if}
-				</td>
-			</tr>
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+	{:catch error}
+		<!-- promise was rejected -->
+		<p>Something went wrong: {error.message}</p>
+	{/await}
 </div>
 
 <style>
